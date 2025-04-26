7733a6e31b365ffc248d40006f8d90cdf03bfed0
muhsinahm <muhsingcu@gmail.com>
2025-04-26 08:17:46 +0500

    Drumkit in Progress

---
+++ DrumKit/drumkit.js
@@ -0,0 +1,40 @@
+import { keysData } from "./keysData.js";
+
+function createButton() {
+  var container = document.getElementById("keys-container");
+  keysData.forEach((item) => {
+    var btn = document.createElement("button");
+    btn.className = "keys";
+
+    var btnText = document.createElement("div");
+    btnText.className = "key-text";
+
+    var keyCharacter = document.createElement("span");
+    keyCharacter.textContent = item?.character;
+
+    const keyName = document.createElement("span");
+    keyName.textContent = item?.name;
+
+    const audioClip = document.createElement("audio");
+    audioClip.setAttribute("src", item?.sound);
+
+    btnText.appendChild(keyCharacter);
+    btnText.appendChild(keyName);
+
+    btn.append(btnText);
+    btn.append(audioClip);
+
+    container.appendChild(btn);
+  });
+  container.addEventListener("click", function (evt) {
+    const button = evt.target.closest("button");
+    if (button && container.contains(button)) {
+      const audio = button.querySelector("audio");
+      if (audio) {
+        audio.currentTime = 0;
+        audio.play();
+      }
+    }
+  });
+}
+createButton();
---
+++ DrumKit/keysData.js
@@ -0,0 +1,11 @@
+export const keysData = [
+  { character: "A", name: "Clap", sound: "./sounds/clap.wav" },
+  { character: "S", name: "Hihat", sound: "./sounds/hihat.wav" },
+  { character: "D", name: "Kick", sound: "./sounds/kick.wav" },
+  { character: "F", name: "Open Hat", sound: "./sounds/openhat.wav" },
+  { character: "G", name: "Boom", sound: "./sounds/boom.wav" },
+  { character: "H", name: "Ride", sound: "./sounds/ride.wav" },
+  { character: "J", name: "Snare", sound: "./sounds/snare.wav" },
+  { character: "K", name: "Tom", sound: "./sounds/tom.wav" },
+  { character: "L", name: "Tink", sound: "./sounds/tink.wav" },
+];
