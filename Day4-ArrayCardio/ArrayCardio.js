b472962e3eb7b39cabe52f025e7939d04387f63a
muhsinahm <muhsingcu@gmail.com>
2025-05-02 02:25:20 +0500

    Array Cardio in Progress

--- Day4-ArrayCardio/ArrayCardio.js
+++ Day4-ArrayCardio/ArrayCardio.js
@@ -1,7 +1,7 @@
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
-import { inventors } from "../Utilities/PersonData.js";
+import { inventors, people } from "../Utilities/PersonData.js";
 
 function findFifteeners() {
   console.log("Inventors", inventors);
@@ -25,3 +25,56 @@ const sortedArray = inventors.sort(function (a, b) {
   return b.year - a.year;
 });
 console.log("Sorted", sortedArray);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+const yearsInventorLived = inventors.reduce(
+  (accumulator, currentValue) =>
+    accumulator + (currentValue.passed - currentValue.year),
+  0
+);
+console.log("Total years they lived: ", yearsInventorLived);
+
+// 5. Sort the inventors by years lived
+
+const oldAged = inventors.sort(function (a, b) {
+  return b.passed - b.year - (a.passed - a.year);
+});
+console.table(oldAged);
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const splitted = people.map((item) => item.split(", "));
+
+const sortedByLastName = splitted.sort((a, b) => a[0].localeCompare(b[0]));
+
+const sortedPeople = sortedByLastName.map((item) => item.join(","));
+
+console.log("Sorted by last name:", sortedPeople);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+  "pogostick",
+];
+const transportationCount = data.reduce((accumulator, item) => {
+  accumulator[item] = (accumulator[item] || 0) + 1;
+  return accumulator;
+}, {});
+
+console.log(transportationCount);
