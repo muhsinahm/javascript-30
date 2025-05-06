ed58d52db5a8d6bcaaf6f4230b389993da7bd73a
muhsinahm <muhsingcu@gmail.com>
2025-05-07 04:09:13 +0500

    FlexImages, text and images added

--- Day5-FlexPanelGallery/FlexPanel.js
+++ Day5-FlexPanelGallery/FlexPanel.js
@@ -1,3 +1,23 @@
+import { FlexPanelImages } from "../Utilities/ImageData.js";
+import { setAttributes } from "../Utilities/UtilityFunctions.js";
+
 const panels = document.createElement("div");
 panels.className = "panel-container";
 document.body.appendChild(panels);
+
+for (let i = 0; i < FlexPanelImages.length; i++) {
+  const panel = document.createElement("div");
+  panel.className = `panel panel${i + 1}`;
+  setAttributes(panel, {
+    style: `background-image:url('${FlexPanelImages[i].image}')`,
+  });
+  for (let key in FlexPanelImages[i]) {
+    if (key.startsWith("text")) {
+      const p = document.createElement("p");
+      p.innerText = FlexPanelImages[i][key];
+      panel.appendChild(p);
+    }
+  }
+
+  panels.appendChild(panel);
+}
---
+++ Utilities/ImageData.js
@@ -0,0 +1,37 @@
+export const FlexPanelImages = [
+  {
+    image:
+      "https://img.freepik.com/free-vector/nature-park-scene-background-with-rainbow-sky_1308-59739.jpg?uid=R18021273&ga=GA1.1.276825037.1740698414&semt=ais_hybrid&w=740",
+    text1: "Hey",
+    text2: "Let's",
+    text3: "Dance",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/blank-landscape-scene-nature-park-sunset-time_1308-59002.jpg?uid=R18021273&ga=GA1.1.276825037.1740698414&semt=ais_hybrid&w=740",
+    text1: "Give",
+    text2: "Take",
+    text3: "Receive",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/blank-landscape-scene-nature-park-sunset-time_1308-61312.jpg?uid=R18021273&ga=GA1.1.276825037.1740698414&semt=ais_hybrid&w=740",
+    text1: "Experience",
+    text2: "It",
+    text3: "Today",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/city-scene-with-tall-buildings_1308-70933.jpg?uid=R18021273&ga=GA1.1.276825037.1740698414&semt=ais_hybrid&w=740",
+    text1: "Give",
+    text2: "All",
+    text3: "You can",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/blank-nature-park-landscape-daytime-scene-with-pathway-through-meadow_1308-63409.jpg?uid=R18021273&ga=GA1.1.276825037.1740698414&semt=ais_hybrid&w=740",
+    text1: "Life",
+    text2: "In",
+    text3: "Motion",
+  },
+];
